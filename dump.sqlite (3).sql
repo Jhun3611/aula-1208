d91b1b4801390ca45fb19f7494fd3f594e1b84de
Jhun3611 <jhunprofissa@gmail.com>
2024-08-12 15:58:39 -0300

    Add files via upload

---
+++ dump.sqlite (3).sql
@@ -0,0 +1,17 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE RESERVAS(
+id INT(5) UNIQUE,
+nome_cliente VARCHAR(30),
+quarto INT(2),
+data_entrada DATE,
+data_saida DATE
+
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
