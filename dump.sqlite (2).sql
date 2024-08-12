6c8a61e5c1153fcc6797fa3f88ef8c56ddf25017
Jhun3611 <jhunprofissa@gmail.com>
2024-08-12 15:29:19 -0300

    Add files via upload

---
+++ dump.sqlite (2).sql
@@ -0,0 +1,21 @@
+-- TABLE
+CREATE TABLE CAMPO(
+  ID INT(4),
+  CIDADE VARCHAR(30) UNIQUE,
+  CAPACIDADE INT (4) NOT NULL
+  
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE produtos(
+  id INT(5) UNIQUE,
+  nome VARCHAR(30),
+  preco DOUBLE(6)
+  
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
