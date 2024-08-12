7d20d46b36e4f04d3fe523f8ac90faa5bebba5f2
Jhun3611 <jhunprofissa@gmail.com>
2024-08-12 13:53:31 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,21 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE funcionarios(
+  nome varchar(255),
+  CPF varchar(11),
+  salario int(5),
+  setor varchar(8),
+  contratacao date(8)
+  );
+CREATE TABLE produtos(
+  descricao varchar(50),
+  preco varchar(20),
+  quantidade varchar(5)
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
